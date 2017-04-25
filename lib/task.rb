a88a57b17a7131bd3aa409bf5bf75d6d417fb3e6
jin camou and Susha Dore <jin@>
2017-04-25 09:12:51 -0700

    add and pass second spec

--- lib/task.rb
+++ lib/task.rb
@@ -7,4 +7,7 @@ class Task
     @description
   end
 
+  define_singleton_method(:all_tasks) do
+    []
+  end  
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -8,4 +8,10 @@ describe(Task) do
       expect(test_task.description()).to(eq("scrub the zebra"))
     end
   end
+
+  describe(".all_tasks") do
+    it("is empty at first") do
+      expect(Task.all_tasks()).to(eq([]))
+    end
+  end
 end
