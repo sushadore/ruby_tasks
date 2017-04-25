9681765c1cf2cc8b44541b09acd817c33cf90d20
jin camou and Susha Dore <jin@>
2017-04-25 09:08:31 -0700

    pass first spec

---
+++ spec/task_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('task')
+
+describe(Task) do
+  describe("#description") do
+    it("lets you give it a description") do
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+end
